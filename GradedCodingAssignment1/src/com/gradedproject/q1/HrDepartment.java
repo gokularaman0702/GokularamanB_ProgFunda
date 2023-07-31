516e70d9f3b8a74015168a49018b2256a3671c43
gokularaman0702 <89916637+gokularaman0702@users.noreply.github.com>
2023-07-31 22:36:44 +0530

    Add files via upload

---
+++ GradedCodingAssignment1/src/com/gradedproject/q1/HrDepartment.java
@@ -0,0 +1,13 @@
+package com.gradedproject.q1;
+
+
+public class HrDepartment extends SuperDepartment{
+
+	public HrDepartment(String departmentName, String getTodaysWork, String getWorkDeadline, String isTodayAHoliday) {
+		super(departmentName, getTodaysWork, getWorkDeadline, isTodayAHoliday);
+	}
+
+	public String doActivity() {
+		return "team Lunch";
+	}
+}
---
+++ GradedCodingAssignment1/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module GradedCodingAssignment1 {
+}
